9bd71d3b8352009b266202255ea37d1125932040
Anna Tyrka <a.m.tyrka@gmail.com>
2021-08-01 10:57:29 +0100

    Crossfade added

--- product_landing_page/assets/js/plantStore.js
+++ product_landing_page/assets/js/plantStore.js
@@ -70,3 +70,29 @@ function changeImg() {
     }
 }
 
+// change plant image
+
+const objImgSrc = {
+    monstera: ["./assets/images/SwissCheese-24cm-2_c1557c95-7882-4478-8103-cb4619b48d91_1_grande.jpg", "./assets/images/Monstera_detail_1_91cb8a6b-1b30-4010-af3f-3cc0caec6c4c_1024x1024@2x.jpg"],
+    ficus: ["./assets/images/Rubber-plant-Robusta-27cm-Weave_grande.jpg", "./assets/images/Rubber-plant-Robusta-27cm-detail_1024x1024@2x.jpg"],
+    snakePlant: ["./assets/images/snake-plant-zeylanica-black-beige-pot_grande.jpg", "./assets/images/sansevieria-zeylanica-close-up-2_1024x1024@2x.jpg"],
+    ceramic: ["./assets/images/cermic_pot.jpg", "./assets/images/ceramic_pot2.jpg"],
+    fractured: ["./assets/images/fractured_pot.jpg", "./assets/images/fractured_pot2.jpg"],
+    clay: ["./assets/images/clay_pot.jpg", "./assets/images/clay_pot2.jpg"]
+};
+
+function mOver(obj) {
+    const objName = obj.getAttribute('id');
+    const objId = document.getElementById(objName);
+    objId.style.opacity = 0.1;
+    setTimeout(function () { obj.setAttribute("src", objImgSrc[objName][1]) }, 400);
+    setTimeout(function () { objId.style.opacity = 1 }, 400);
+}
+
+function mOut(obj) {
+    const objName = obj.getAttribute('id');
+    const objId = document.getElementById(objName);
+    objId.style.opacity = 0.1;
+    setTimeout(function () { obj.setAttribute("src", objImgSrc[objName][0]) }, 400);
+    setTimeout(function () { objId.style.opacity = 1 }, 400);
+}
